838c3f524ab4a26c45f326347ba1c697f33738cd
yassinem118 <yassineboss436@gmail.com>
2026-08-14 19:34:19 +0100

    Add SimpleStorage contract with set and get functions

---
+++ SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.28;
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() external view returns (uint) {
+        return storedData;
+    }
+}
